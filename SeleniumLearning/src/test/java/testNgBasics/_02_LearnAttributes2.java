6165aa927d5ff303141b0f6fab889e2b0b343169
SeleniumLearning <kinnesperumal09@gmail.com>
2025-07-25 22:53:19 +0530

    Lean Attributes

---
+++ SeleniumLearning/src/test/java/testNgBasics/_02_LearnAttributes2.java
@@ -0,0 +1,27 @@
+package testNgBasics;
+
+import org.testng.annotations.Test;
+
+public class _02_LearnAttributes2 {
+
+	@Test(priority = -1)
+	public void singUp() {
+		System.out.println("signUp");
+	}
+	@Test()//it convert 0 priority
+	public void login() {
+		System.out.println("login");
+	}
+	@Test(priority = 3)
+	public void searchProduct() {
+		System.out.println("searchProduct");
+	}
+	@Test(priority = 4)
+	public void addToCart() {
+		System.out.println("addToCart");
+	}
+	@Test(priority = -5)
+	public void signOut() {
+		System.out.println("signOut");
+	}
+}
