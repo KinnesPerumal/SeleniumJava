48f96995fc834ebd33e48310f7dee53abaa45fb1
SeleniumLearning <kinnesperumal09@gmail.com>
2025-07-26 00:01:00 +0530

    lern attributes

--- SeleniumLearning/src/test/java/testNgBasics/_04_LearnAttributes4.java
+++ SeleniumLearning/src/test/java/testNgBasics/_04_LearnAttributes4.java
@@ -1,18 +1,21 @@
 package testNgBasics;
 
+import java.util.NoSuchElementException;
+
 import org.testng.annotations.Test;
 
 public class _04_LearnAttributes4 {
 
-	@Test(priority = 1)
+	@Test(enabled = false, alwaysRun = true)
 	public void signUp() {
 		System.out.println("signUp");
 	}
-	@Test()
+	@Test(description = "Login sucessfully")
 	public void login() {
 		System.out.println("login");
+		throw new NoSuchElementException();
 	}
-	@Test(dependsOnMethods = "login")
+	@Test(dependsOnMethods = "login", alwaysRun = true)
 	public void searchProduct() {
 		System.out.println("searchProduct");
 	}
