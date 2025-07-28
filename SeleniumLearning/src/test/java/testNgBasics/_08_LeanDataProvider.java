4fe1a6cc570943ccc4351362ff7d6a2ce8f55de9
SeleniumLearning <kinnesperumal09@gmail.com>
2025-07-28 21:55:21 +0530

    Parameters

--- SeleniumLearning/src/test/java/testNgBasics/_08_LeanDataProvider.java
+++ SeleniumLearning/src/test/java/testNgBasics/_08_LeanDataProvider.java
@@ -7,19 +7,13 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-public class _08_LeanDataProvider {
-	@DataProvider(name = "loginData")
-	public String[][] getData() {
-		String [][] data = new String[2][2];
-		data[0][0] = "student";
-		data[0][1] = "Password123";
-		data[1][0] = "student";
-		data[1][1] = "Password123";
-		return data;
-		
-	}
-	@Test(dataProvider = "loginData")
-	public void test(String uName, String Password) {
+import utils._01_DataUtils;
+
+public class _08_LeanDataProvider {//extends _01_DataUtils we can use this way for 1 method
+	
+	@Test(dataProvider = "loginData", dataProviderClass = _01_DataUtils.class)//dirtly pass methodname for dataprovier name
+	public void test(String uName, String Password) {//inside parameter can use like, String data[]
+//		System.out.println(data[0]);
 		System.setProperty("webdriver.chrome.driver", "./Drivers/chromedriver.exe");
 		ChromeDriver driver = new ChromeDriver();
 		driver.get("https://practicetestautomation.com/practice-test-login/");
---
+++ SeleniumLearning/src/test/java/utils/_01_DataUtils.java
@@ -0,0 +1,18 @@
+package utils;
+
+import org.testng.annotations.DataProvider;
+
+public class _01_DataUtils {
+	
+	@DataProvider(name = "loginData", indices = {1,2}, parallel = true)
+	public String[][] getData() {
+		String [][] data = new String[3][2];
+		data[0][0] = "student";
+		data[0][1] = "Password123";
+		data[1][0] = "student";
+		data[1][1] = "Password123";
+		data[2][0] = "tudent";
+		data[2][1] = "Password123";
+		return data;
+	}
+}
