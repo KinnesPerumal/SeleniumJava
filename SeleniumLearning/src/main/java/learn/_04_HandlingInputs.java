b1e46beab9e99d9f852e9915420a827cf6cc2fe8
SeleniumLearning <kinnesperumal09@gmail.com>
2025-07-29 23:32:26 +0530

    learn

--- SeleniumLearning/src/main/java/learn/_04_HandlingInputs.java
+++ SeleniumLearning/src/main/java/learn/_04_HandlingInputs.java
@@ -16,22 +16,22 @@ public class _04_HandlingInputs {
 		//maximizing browser
 		driver.manage().window().maximize();
 		//enter full name
-		driver.findElement(By.id("fullName")).sendKeys("Kinnes");
-		//append a text
-		driver.findElement(By.id("join")).sendKeys(" Person",Keys.TAB);//it is appending
-		//get a text
-		String myValue =  driver.findElement(By.id("getMe")).getAttribute("value");
-		System.out.println(myValue);
-		//clear the text
-		driver.findElement(By.id("clearMe")).clear();
-		//confirm the edit field is disable
-		boolean isEdit = driver.findElement(By.id("noEdit")).isEnabled();
-		System.out.println(isEdit);
-		//confirm text is read only
-		String isReadOnly= driver.findElement(By.id("dontwrite")).getAttribute("readonly");
-		System.out.println(isReadOnly);
-		//quite browser
-//		driver.quit();
+//		driver.findElement(By.id("fullName")).sendKeys("Kinnes");
+//		//append a text
+//		driver.findElement(By.id("join")).sendKeys(" Person",Keys.TAB);//it is appending
+//		//get a text
+//		String myValue =  driver.findElement(By.id("getMe")).getAttribute("value");
+//		System.out.println(myValue);
+//		//clear the text
+//		driver.findElement(By.id("clearMe")).clear();
+//		//confirm the edit field is disable
+//		boolean isEdit = driver.findElement(By.id("noEdit")).isEnabled();
+//		System.out.println(isEdit);
+//		//confirm text is read only
+//		String isReadOnly= driver.findElement(By.id("dontwrite")).getAttribute("readonly");
+//		System.out.println(isReadOnly);
+//		//quite browser
+////		driver.quit();
 
 	}
 
